5470ccbbfc46a75568b74d241c22cf8a72ccf4d9
Muil Yang <muilyang12@gmail.com>
2025-03-08 14:41:09 -0600

    Comment out unused functions.

--- utils/loss_utils.py
+++ utils/loss_utils.py
@@ -13,35 +13,35 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 from math import exp
-try:
-    from diff_gaussian_rasterization._C import fusedssim, fusedssim_backward
-except:
-    pass
+# try:
+#     from diff_gaussian_rasterization._C import fusedssim, fusedssim_backward
+# except:
+#     pass
 
 C1 = 0.01 ** 2
 C2 = 0.03 ** 2
 
-class FusedSSIMMap(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, C1, C2, img1, img2):
-        ssim_map = fusedssim(C1, C2, img1, img2)
-        ctx.save_for_backward(img1.detach(), img2)
-        ctx.C1 = C1
-        ctx.C2 = C2
-        return ssim_map
+# class FusedSSIMMap(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, C1, C2, img1, img2):
+#         ssim_map = fusedssim(C1, C2, img1, img2)
+#         ctx.save_for_backward(img1.detach(), img2)
+#         ctx.C1 = C1
+#         ctx.C2 = C2
+#         return ssim_map  
 
-    @staticmethod
-    def backward(ctx, opt_grad):
-        img1, img2 = ctx.saved_tensors
-        C1, C2 = ctx.C1, ctx.C2
-        grad = fusedssim_backward(C1, C2, img1, img2, opt_grad)
-        return None, None, grad, None
+#     @staticmethod
+#     def backward(ctx, opt_grad):
+#         img1, img2 = ctx.saved_tensors
+#         C1, C2 = ctx.C1, ctx.C2
+#         grad = fusedssim_backward(C1, C2, img1, img2, opt_grad)
+#         return None, None, grad, None
 
 def l1_loss(network_output, gt):
     return torch.abs((network_output - gt)).mean()
 
-def l2_loss(network_output, gt):
-    return ((network_output - gt) ** 2).mean()
+# def l2_loss(network_output, gt):
+#     return ((network_output - gt) ** 2).mean()
 
 def gaussian(window_size, sigma):
     gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
@@ -86,6 +86,6 @@ def _ssim(img1, img2, window, window_size, channel, size_average=True):
         return ssim_map.mean(1).mean(1).mean(1)
 
 
-def fast_ssim(img1, img2):
-    ssim_map = FusedSSIMMap.apply(C1, C2, img1, img2)
-    return ssim_map.mean()
+# def fast_ssim(img1, img2):
+#     ssim_map = FusedSSIMMap.apply(C1, C2, img1, img2)
+#     return ssim_map.mean()
